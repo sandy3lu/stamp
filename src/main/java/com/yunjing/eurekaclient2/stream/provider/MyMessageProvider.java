816c8ffcf43654e3da0e13265f72e4ef9f3f60d1
lurong <sandy3lu@126.com>
2019-02-01 17:43:40 +0800

    delete rabbit

--- src/main/java/com/yunjing/eurekaclient2/stream/provider/MyMessageProvider.java
+++
@@ -1,7 +0,0 @@
-package com.yunjing.eurekaclient2.stream.provider;
-
-import com.yunjing.eurekaclient2.web.entity.DictConstant;
-
-public interface MyMessageProvider {
-    void send(DictConstant dictConstant);
-}
--- src/test/java/com/yunjing/eurekaclient2/EurekaClient2ApplicationTests.java
+++ src/test/java/com/yunjing/eurekaclient2/EurekaClient2ApplicationTests.java
@@ -1,7 +1,6 @@
 package com.yunjing.eurekaclient2;
 
-import com.yunjing.eurekaclient2.stream.provider.IMessageProvider;
-import com.yunjing.eurekaclient2.stream.provider.MyMessageProvider;
+
 import com.yunjing.eurekaclient2.web.entity.DictConstant;
 import org.junit.Assert;
 import org.junit.Test;
@@ -29,11 +28,7 @@ public class EurekaClient2ApplicationTests {
     public void contextLoads() {
     }
 
-    @Resource
-    private IMessageProvider messageProvider;
 
-    @Resource
-    private MyMessageProvider myMessageProvider;
 
     /**
      * stream 测试
@@ -43,8 +38,7 @@ public class EurekaClient2ApplicationTests {
         DictConstant dictConstant = new DictConstant();
         dictConstant.setId(1); // 继承的属性值不发送
         dictConstant.setValue("hello world !");
-        messageProvider.send(dictConstant);
-        myMessageProvider.send(dictConstant);
+
     }
 
     /***
